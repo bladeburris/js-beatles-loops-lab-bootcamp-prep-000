cbfbb29c7bdb4dadeab0594f5227f5886a66f4b4
blade burris <bladeburris@gmail.com>
2018-04-16 19:08:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = []
+  for (var i = 0;  i < musicians.length; i++) {
+    var musician = musicians[i]
+    var instrument = instruments[i]
+    var fact = `${musician} plays ${instrument}`
+    newArray.push(fact)
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts) {
+  var newArray = []
+  var n = 0
+  while (n < facts.length) {
+    var newString = facts[0] + "!!!"
+    newArray.push(newString)
+  }
+  return newArray
+}
